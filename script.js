1c60c899823aed62e1937d5f76beb15979a82d56
Volkov Dmitriy <vdy0555@gmail.com>
2023-03-26 12:07:06 +0700

    deleted routes

--- script.js
+++
@@ -1,8 +0,0 @@
-import { route } from "./router.js"
-
-const pageHrefs = document.querySelectorAll('a[data-page-href]')
-debugger
-
-pageHrefs.forEach(href => {
-  href.addEventListener('click', route)
-})
