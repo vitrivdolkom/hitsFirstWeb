8bb9d9d360e866d75d4419e466dc27dc6a3f28ac
Volkov Dmitriy <vdy0555@gmail.com>
2023-04-20 08:34:32 +0700

    main page adaptive layout without modal text

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
 import { openModal } from './modal.js'
 
-const toModal = document.querySelectorAll('.openModal')
+const astarInfo = document.querySelector('div[data-algo="astar"]')
+const antsInfo = document.querySelector('div[data-algo="ants"]')
+const treeInfo = document.querySelector('div[data-algo="tree"]')
+const clusterInfo = document.querySelector('div[data-algo="cluster"]')
+const network = document.querySelector('div[data-algo="network"]')
+const genetic = document.querySelector('div[data-algo="genetic"]')
 
-toModal.forEach((link) => {
-    link.addEventListener('click', () => {
-        openModal('f;jsdlfkdf')
-    })
+astarInfo.addEventListener('click', () => {
+    openModal('Информация об аСтаре')
+})
+
+antsInfo.addEventListener('click', () => {
+    openModal('Информация об муравьях')
+})
+
+treeInfo.addEventListener('click', () => {
+    openModal('Информация об дереве')
+})
+
+clusterInfo.addEventListener('click', () => {
+    openModal('Информация об кластере')
+})
+
+network.addEventListener('click', () => {
+    openModal('Информация об нейронке')
+})
+
+genetic.addEventListener('click', () => {
+    openModal('Информация об генетике')
 })
